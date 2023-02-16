6b96bac14b49e52bb138d29f88b275ad973fc098
2220372 <2220372>
2023-02-16 14:06:46 +0900

    commit 1

--- app/src/main/java/com/example/easytolearn/MainActivity.kt
+++ app/src/main/java/com/example/easytolearn/MainActivity.kt
@@ -10,24 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //idを取得
-        var tx:TextView = findViewById(R.id.tv)
-        var btn1:Button = findViewById(R.id.dog)
-        var btn2:Button = findViewById(R.id.cat)
-        var btn3: Button = findViewById(R.id.clear)
-
-
-
-        //クリック処理
-        btn1.setOnClickListener {
-            tx.text="いぬ"
-        }
-        btn2.setOnClickListener {
-            tx.text="ねこ"
-        }
-        btn3.setOnClickListener {
-            tx.text="..."
-        }
 
     }
 }
