18ccc1512d10a2c8d286d5ff6be23cceefe77c84
2220372 <2220372>
2023-02-16 16:07:01 +0900

    3

--- app/src/main/java/com/example/easytolearn/category.kt
+++ app/src/main/java/com/example/easytolearn/category.kt
@@ -1,7 +1,9 @@
 package com.example.easytolearn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class category : AppCompatActivity() {
@@ -9,6 +11,23 @@ class category : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
 
+        val greetBtn = findViewById<Button>(R.id.greets)
+        val numberBtn = findViewById<Button>(R.id.numbers)
+        val fruitsBtn = findViewById<Button>(R.id.fruits)
+
+
+        greetBtn.setOnClickListener {
+            val intent = Intent(this,course::class.java)
+            startActivity(intent)
+        }
+        numberBtn.setOnClickListener {
+            val intent = Intent(this,course::class.java)
+            startActivity(intent)
+        }
+        fruitsBtn.setOnClickListener {
+            val intent = Intent(this,course::class.java)
+            startActivity(intent)
+        }
 
 
 
