48e7af6052fe6f1b0c2a7d061c09f4276fdabd84
2220372 <2220372>
2023-02-16 16:32:03 +0900

    course

--- app/src/main/java/com/example/easytolearn/course.kt
+++ app/src/main/java/com/example/easytolearn/course.kt
@@ -7,5 +7,6 @@ class course : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course)
+
     }
 }
